6c4f0fe604575c5332c8a1ea13ace625383bed62
Artem Kartashev <seokiller2011@gmail.com>
2022-11-10 23:26:46 +0300

    add solution method

--- Solution/Program.cs
+++ Solution/Program.cs
@@ -2,3 +2,16 @@
 
 string[] arrayoriginal = new string[7] {"Hope", "1234", "777", "Bright future", "Yes", "Lucky", "Matilda"};
 string[] arrayfinal = new string[arrayoriginal.Length];
+
+void ArrayFinalSolution(string[] arrayoriginal, string[] arrayfinal)
+{
+    int count = 0;
+    for (int i = 0; i < arrayoriginal.Length; i++)
+    {
+        if (arrayoriginal.Length <=3)
+        {
+            arrayfinal[count] = arrayoriginal[i];
+            count++;
+        }
+    }
+}
