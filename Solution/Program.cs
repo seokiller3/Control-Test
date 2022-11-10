5bbe9dd363f5a4ed54839c6c481a9b894679bc95
Artem Kartashev <seokiller2011@gmail.com>
2022-11-10 23:18:10 +0300

    first algorithm

---
+++ Solution/Program.cs
@@ -0,0 +1,4 @@
+﻿Console.Clear();
+
+string[] arrayoriginal = new string[7] {"Hope", "1234", "777", "Bright future", "Yes", "Lucky", "Matilda"};
+string[] arrayfinal = new string[arrayoriginal.Length];
